620dfcd7f2aaf5b96b9abec5eadda25c9847d4e8
Vinícius Sobreira <vsm4@cin.ufpe.br>
2023-12-14 15:55:52 -0300

    Agora vai? Subindo a calculadora pro GitHub
    Já tentei tanto, errei tanto, será que agora vai?

---
+++ calculadora_JS/functionsCalc.mjs
@@ -0,0 +1,19 @@
+export function soma (a,b) {
+    return a+b;
+}
+
+export function subtracao (a,b) {
+    return a-b;
+}
+
+export function multiplicacao (a,b) {
+    return a*b;
+}
+
+export function divisao (a,b) {
+    return a/b;
+}
+
+export function exponenciacao (a,b) {
+    return a**b;
+}
